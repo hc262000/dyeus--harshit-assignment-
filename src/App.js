efdebc4ba1d3576e2f96131bc49c11ab706e292d
harshit <harshitchaudhary20@gmail.com>
2022-03-22 18:46:21 +0530

    page1 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import NavBar from "./components/navbar"
+import Body1 from "./componenets/body1"
 function App() {
   return (
     <>
     <NavBar/>
+    <Body1/>
     </>
    
   );
